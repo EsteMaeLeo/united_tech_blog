3b78f272d26f1e1d926c9d998733d4f8627eab37
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-31 19:49:03 -0500

    adding javascript files public folder, code, and partials and single post and styles in css file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,12 +27,12 @@ router.get("/", (req, res) => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
       console.log(posts);
       //console.log(dbPostData[0]);
-      res.render("homepage", { posts });
+      //res.render("homepage", { posts });
       //res.render("homepage", dbPostData[1].get({ plain: true }));
-      //   res.render("homepage", {
-      //     posts,
-      //     loggedIn: req.session.loggedIn,
-      //   });
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch(err => {
       console.log(err);
@@ -41,11 +41,53 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect("/");
-  //   return;
-  // }
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
   res.render("login");
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "post_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
